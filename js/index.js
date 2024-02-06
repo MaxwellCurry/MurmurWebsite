2f9e7ee839be7911c03f84937002ff4a44769fa9
Maxwell Fung <maxwellfung1@gmail.com>
2024-02-05 17:06:02 -0800

    your message

--- js/index.js
+++ js/index.js
@@ -1,32 +1,25 @@
-window.addEventListener("DOMContentLoaded", (event) => {
-    const donate = document.getElementById("donate-button");
-		if (donate) {
-			donate.addEventListener("click", function() {
-				window.open("https://midpenpost.org/tag/garv-virginkar/", "_blank");
-			});
-		}
-});
 document.addEventListener("DOMContentLoaded", function(){
   var searchInput = document.querySelector(".search-bar");
   var dropdown = document.querySelector(".schools-dropdown");
-	var schoolOptions = document.querySelectorAll(".school-option");
 
-  for(let i = 0; i < 6000; i++) {
+
+  for (let i = 0; i < 6000; i++) {
 		var option = document.createElement("div");
 		option.classList.add("school-option");
 		dropdown.appendChild(option);
   }
+  var schoolOptions = document.querySelectorAll(".school-option");
 
-  fetch('schools.txt')
+  fetch('/schools.txt')
 	.then(response => response.text())
 	.then(data => {
 		var schools = data.split('\n').map(function(item) {
 			return item.trim();
-		});
-		schoolOptions.forEach(function(option, index) {
+	  });
+	  schoolOptions.forEach(function(option, index) {
 			option.textContent = schools[index];
-		});
-	}).catch(error => console.error('Error fetching schools:', error));
+	  });
+  }).catch(error => console.error('Error fetching schools:', error));
 
   document.addEventListener("click", function (event) {
 		if (event.target.closest(".search-bar-container")) {
@@ -35,13 +28,12 @@ document.addEventListener("DOMContentLoaded", function(){
 				if (index < 5) {
 					option.style.display = "block";
 				} else {
-				 option.style.display = "none";
+					option.style.display = "none";
 				}
 			});
 		} else {
 			dropdown.style.display = "none";
 		}
-
 		if (event.target.classList.contains("school-option")) {
 			searchInput.value = event.target.textContent;
 			dropdown.style.display = "none";
@@ -63,6 +55,17 @@ document.addEventListener("DOMContentLoaded", function(){
 		});
   });
 });
+
+function redirectToSignup() {
+  var schoolSearchInput = document.getElementById("schoolSearch");
+  var schoolName = schoolSearchInput.value.trim();
+  window.location.href = "/html/signup.html?school=" + encodeURIComponent(schoolName);
+}
+
+function redirectToLogin() {
+  window.location.href = "login.html";
+}
+
 document.addEventListener("DOMContentLoaded", function () {
 	var infoButton = document.getElementById("info-button");
 	var popupContainer = document.createElement("div");
@@ -84,16 +87,14 @@ document.addEventListener("DOMContentLoaded", function () {
 	closePopup.addEventListener("click", function () {
 	  popupContainer.style.display = "none";
 	});
-infoButton.addEventListener("click", function () {
-		// Toggle the display property
+
+	infoButton.addEventListener("click", function () {
 		popupContainer.style.display = popupContainer.style.display === "none" ? "block" : "none";
 	});
 });
-function redirectToSignup() {
-  var schoolSearchInput = document.getElementById("schoolSearch");
-  var schoolName = schoolSearchInput.value.trim();
-  window.location.href = "/html/signup.html?school=" + encodeURIComponent(schoolName);
-}
-function redirectToLogin() {
-  window.location.href = "/html/login.html";
-}
+
+
+
+document.getElementById("donate-button").addEventListener("click", function() {
+	window.open("https://midpenpost.org/tag/garv-virginkar/", "_blank");
+});
